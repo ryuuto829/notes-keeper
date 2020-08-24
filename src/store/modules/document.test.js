84a32cce88232332e02c27db85372bca56b42ca3
ryuuto829 <ryuuto829@gmail.com>
2020-08-24 19:06:12 +0300

    test: write new tests for document reducer
    * Add test for moveUp, moveDown and mergeItem
    * Cleanup tests

--- src/store/modules/document.test.js
+++ src/store/modules/document.test.js
@@ -2,7 +2,10 @@ import document, {
   removeEditable,
   setEditable,
   updateContent,
-  addItem
+  addItem,
+  moveUp,
+  moveDown,
+  mergeItem
 } from "./document";
 
 describe("document reducer", () => {
@@ -49,7 +52,7 @@ describe("document reducer", () => {
     });
   });
 
-  test("should create a new item, give it focus when created as a child", () => {
+  test("should add a new item as child, give it focus", () => {
     expect(
       document(
         {
@@ -79,7 +82,7 @@ describe("document reducer", () => {
     });
   });
 
-  test("should create a new item, give it focus when created as a child", () => {
+  test("should add a new item as sibling, give it focus", () => {
     expect(
       document(
         {
@@ -112,6 +115,188 @@ describe("document reducer", () => {
           content: "",
           children: null,
           parent: "id1"
+        }
+      }
+    });
+  });
+
+  test("should move item up if lvl > 1, give it focus", () => {
+    expect(
+      document(
+        {
+          collection: {
+            id1: {
+              children: ["id2"],
+              level: 0
+            },
+            id2: {
+              parent: "id1",
+              children: ["id4"],
+              level: 1
+            },
+            id4: {
+              parent: "id2",
+              children: null,
+              level: 2
+            }
+          }
+        },
+        { type: moveUp.type, payload: { currentId: "id4" } }
+      )
+    ).toEqual({
+      editable: "id4",
+      collection: {
+        id1: {
+          children: ["id2", "id4"],
+          level: 0
+        },
+        id2: {
+          parent: "id1",
+          children: null,
+          level: 1
+        },
+        id4: {
+          parent: "id1",
+          children: null,
+          level: 1
+        }
+      }
+    });
+  });
+
+  test("should move item down if appended as a sibling", () => {
+    expect(
+      document(
+        {
+          editable: "id4",
+          collection: {
+            id1: {
+              children: ["id2", "id4"],
+              level: 0
+            },
+            id2: {
+              parent: "id1",
+              children: null,
+              level: 1
+            },
+            id4: {
+              parent: "id1",
+              children: null,
+              level: 1
+            }
+          }
+        },
+        { type: moveDown.type, payload: { currentId: "id4" } }
+      )
+    ).toEqual({
+      editable: "id4",
+      collection: {
+        id1: {
+          children: ["id2"],
+          level: 0
+        },
+        id2: {
+          parent: "id1",
+          children: ["id4"],
+          level: 1
+        },
+        id4: {
+          parent: "id2",
+          children: null,
+          level: 2
+        }
+      }
+    });
+  });
+
+  test("should don't move down if it's first child", () => {
+    const initialState = {
+      collection: {
+        id1: {
+          children: ["id4"],
+          level: 0
+        },
+        id4: {
+          parent: "id1",
+          children: null,
+          level: 1
+        }
+      }
+    };
+    expect(
+      document(initialState, {
+        type: moveDown.type,
+        payload: { currentId: "id4" }
+      })
+    ).toEqual(initialState);
+  });
+
+  test("shouldn't merge item with own children", () => {
+    const initialState = {
+      collection: {
+        id2: {
+          children: ["id4"],
+          level: 1
+        },
+        id4: {
+          parent: "id2",
+          children: ["id6"],
+          level: 2
+        }
+      }
+    };
+    expect(
+      document(initialState, {
+        type: mergeItem.type,
+        payload: { currentId: "id4" }
+      })
+    ).toEqual(initialState);
+  });
+
+  test("should merge child with parent and append children from it", () => {
+    expect(
+      document(
+        {
+          collection: {
+            id2: {
+              children: ["id3", "id4"],
+              level: 1
+            },
+            id3: {
+              content: "Parent text",
+              children: null,
+              level: 2
+            },
+            id4: {
+              content: " Child text",
+              parent: "id2",
+              children: ["id6"],
+              level: 2
+            },
+            id6: {
+              parent: "id4",
+              children: ["id7"],
+              level: 3
+            }
+          }
+        },
+        { type: mergeItem.type, payload: { currentId: "id4" } }
+      )
+    ).toEqual({
+      collection: {
+        id2: {
+          children: ["id3"],
+          level: 1
+        },
+        id3: {
+          content: "Parent text Child text",
+          children: ["id6"],
+          level: 2
+        },
+        id6: {
+          parent: "id3",
+          children: ["id7"],
+          level: 3
         }
       }
     });
